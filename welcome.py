1ff56b0a500e8b11167b7d86fa4469f908b7fa06
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-05-30 10:44:37 +0300

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -59,7 +59,7 @@ def welcome_page():
         .music-image {{
             max-width: 100%;
             max-height: 100px;
-            margin-bottom: 5px;
+            margin-bottom: 2px;
         }}
         .footer-note {{
             font-size: 10px;
