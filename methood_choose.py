d071946080f3c8ff1f0ba49f76565aa0c39712e9
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-05-31 18:54:52 +0300

    Add files via upload

--- methood_choose.py
+++ methood_choose.py
@@ -48,7 +48,7 @@ def method_choose_page():
             }
             .sub-header-text {
                 color: #CCCCCC;
-                font-size: 17px;
+                font-size: 14px;
                 margin: 0px;
                 font-weight: bold;
             }
