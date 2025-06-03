eab453a930c76b3e2e095d8c337dbbd239e8f338
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-03 16:57:51 +0300

    Add files via upload

--- user_classification_intro.py
+++ user_classification_intro.py
@@ -34,7 +34,7 @@ def user_classification_intro_page():
             color: white;
             border-radius: 25px;
             padding: 8px;
-            padding-bottom: 10px;
+            padding-bottom: 0px;
             text-align: center;
             box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
             max-width: 400px;
