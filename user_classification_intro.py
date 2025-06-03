0d409a9df4855abe7ac2230019447eace5abc71a
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-03 16:56:13 +0300

    Add files via upload

--- user_classification_intro.py
+++ user_classification_intro.py
@@ -111,6 +111,7 @@ def user_classification_intro_page():
                 border: none;
                 cursor: pointer;
                 border-radius: 50%;
+                margin-bottom: 0px;
                 transition: transform 0.6s ease-in-out, box-shadow 0.3s;
                 box-shadow: 0px 0px 20px rgba(255, 255, 255, 0.5);
                 background-image: url('data:image/webp;base64,""" + lets_go + """');
