a06fb361bc3215069cd42c11beb1747cfe5abffe
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-03 18:37:22 +0300

    Add files via upload

--- thank_you.py
+++ thank_you.py
@@ -54,8 +54,8 @@ def thank_you_page():
             .thank-you-message {
                 font-size: 19px;
                 color: #444;
-                margin-bottom: 10px;
-                padding: 0 15px;
+                margin-bottom: 20px;
+                padding: 3px 15px;
             }
             .download-button a {
                 display: inline-block;
