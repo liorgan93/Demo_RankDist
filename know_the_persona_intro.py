afa4f0da26a0c73281b4d82394cec3c7028ff2e8
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-03 17:17:25 +0300

    Add files via upload

--- know_the_persona_intro.py
+++ know_the_persona_intro.py
@@ -66,7 +66,7 @@ def know_the_persona_intro_page():
             color: #ffffff;
         }
         .sub-header {
-            font-size: 20px;
+            font-size: 17px;
             font-weight: 500;
             color: #ffffff;
             font-family: 'Verdana', sans-serif; 
