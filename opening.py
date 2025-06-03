4ed27b63169200eb8805f3dff6d5fb5f25228154
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-03 16:44:00 +0300

    Add files via upload

--- opening.py
+++ opening.py
@@ -45,11 +45,11 @@ def opening_page():
             color: #BBDEFB;
             font-weight: 550;
             text-shadow: 1px 1px 5px rgba(100, 180, 255, 0.4);
-            margin-bottom: 22px;
+            margin-bottom: 40px;
             padding-top: 5px;
         }}
         .info-text-secondary {{
-            font-size: 20px;
+            font-size: 21px;
             color: #4DD0E1;
             font-weight: 700;
             text-shadow: 1px 1px 6px rgba(0, 200, 255, 0.5);
