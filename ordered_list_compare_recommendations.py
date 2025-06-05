c444921c493b073bbf8c6a443d5ae4318931010b
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-05 19:05:01 +0300

    Add files via upload

--- ordered_list_compare_recommendations.py
+++ ordered_list_compare_recommendations.py
@@ -88,8 +88,9 @@ def ordered_list_compare_recommendations_page():
         """,
         unsafe_allow_html=True
     )
+
     st.markdown('<div class="title-text">Comparison and Evaluation</div>', unsafe_allow_html=True)
-    st.markdown('<div style="text-align:center; margin-top:-28px; font-size:18px; text-shadow: 1px 1px 6px rgba(0, 150, 255, 0.6);">Your picks vs. the RankDist algorithm</div>',unsafe_allow_html=True)
+    st.markdown('<div style="text-align:center; margin-top:-28px; font-size:18px; font-weight: bold;">Your picks vs. the RankDist algorithm</div>',unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns([0.05, 0.9, 0.05])
     with col2:
