8d5240b511dc2142b74dc592672dac239587f794
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-07 13:21:34 +0300

    Add files via upload

--- persona_reveal.py
+++ persona_reveal.py
@@ -16,7 +16,7 @@ def persona_reveal_page():
     st.session_state.songs_df['like/dislike'] = st.session_state.song_feedback
     st.session_state.songs_df['weights'] = st.session_state.songs_df['weights'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
     st.session_state.chosen_person_number, scores = classify_user_by_preferences(st.session_state.songs_df)
-    st.markdown(st.session_state.songs_df)
+    st.dataframe(st.session_state.songs_df)
     if "persona" not in st.session_state:
         names = pd.read_csv("playlists_excel/names.csv")
         names_list = names[names["cluster"] == st.session_state.chosen_person_number]["name"].tolist()
