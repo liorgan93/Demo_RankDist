55a0e63ab4c2a0eab1ebe6ad4491e615308f6204
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-18 10:22:17 +0300

    Add files via upload

--- thank_you.py
+++ thank_you.py
@@ -31,7 +31,7 @@ def thank_you_page():
             .thank-you-container {
                 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
                 text-align: center;
-                padding: 40px 10px;
+                padding: 30px 10px;
                 background: linear-gradient(135deg, rgba(253, 253, 255, 1), rgba(245, 248, 255, 1));
                 border-radius: 30px;
                 box-shadow: 0 10px 25px rgba(0, 0, 0, 0.25);
