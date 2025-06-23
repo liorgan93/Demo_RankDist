15024b2844c4c2fabcb566773339620b2da28ce4
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-23 23:58:18 +0300

    Add files via upload

--- perfect_precision_choose.py
+++ perfect_precision_choose.py
@@ -13,7 +13,7 @@ def render_song_with_fallback_dynamic(embed_url: str, idx: int, height=85):
         </div>
 
         <!-- Error Message and Retry Button -->
-        <div id="error-msg{idx}" style="display: none; height: {height}px; display: flex; flex-direction: column; align-items: center; justify-content: flex-start; padding-top: 3px; gap: 1px;">
+        <div id="error-msg{idx}" style="display: none; height: {height}px; display: flex; flex-direction: column; align-items: center; justify-content: flex-start; padding-top: 0px; gap: 5px;">
             <p style="margin:10px; font-size:14px; font-weight:600; color:#fff; font-family:Arial, sans-serif;">Oops! The song failed to load</p>
             <div onclick="reloadIframe{idx}()" class="try-again-button">
                 <div class="try-text">⟳</div>
