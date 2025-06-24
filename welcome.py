2d9503ec3bb0589e30ee307bdbb6d1d151804b1a
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-24 11:50:13 +0300

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -68,6 +68,7 @@ def welcome_page():
             margin-bottom: 0px;
             padding-bottom: 0px;
             height: 70px;
+            
             width: auto;
             object-fit: contain;
 
