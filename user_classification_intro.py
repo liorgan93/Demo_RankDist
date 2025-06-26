c42b3cab2e036443d32484dbd2b53e8162414fb2
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-06-26 10:24:35 +0300

    Add files via upload

--- user_classification_intro.py
+++ user_classification_intro.py
@@ -73,7 +73,7 @@ def user_classification_intro_page():
         </style>
         <div class="container">
             <div class="header"> Like or Dislike? </div>
-            <div class="sub-header">Let’s start by<strong style="color: #a85fcb"> getting familiar </strong> with your <strong style="color: #a85fcb"> musical taste! </strong> </div>
+            <div class="sub-header">Let’s start by<strong style="color: #f0d1ff"> getting familiar </strong> with your <strong style="color: #f0d1ff"> musical taste! </strong> </div>
             <div class="description">
                 <span class="green-text">Like the song?</span>&nbsp;&nbsp;Tap 👍 <br>
                 <span class="red-text">Not your vibe?</span>&nbsp;&nbsp;Tap 👎
